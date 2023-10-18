f846dacff9b82a6ec3acb20634ab27a41044a341
sangwoong <timeattacks2@naver.com>
2023-10-18 09:43:04 +0900

    FEAT : 프로젝트 환경 재설정 및 REST DOCS 적용

--- blooming/src/main/resources/_sql.sql
+++ blooming/src/main/resources/_sql.sql
@@ -1,14 +1,15 @@
 DROP DATABASE IF EXISTS project;
+
 CREATE DATABASE IF NOT EXISTS project;
 
 USE project;
 
 CREATE TABLE `employee`
 (
-    `employee_no` BIGINT      NOT NULL,
+    `employee_no` BIGINT      NOT NULL PRIMARY KEY,
     `id`          VARCHAR(20) NOT NULL UNIQUE,
     `password`    VARCHAR(20) NOT NULL,
-    `name`        VARCHAR(10) NOT NULL,
+    `name`        VARCHAR(30) NOT NULL,
     `img`         BLOB        NULL,
     `role`        TINYINT(1)  NOT NULL DEFAULT 1,
     `tel`         VARCHAR(13) NULL,
@@ -17,12 +18,14 @@ CREATE TABLE `employee`
 
 CREATE TABLE `product`
 (
-    `product_no`   BIGINT      NOT NULL,
-    `product_code` VARCHAR(5)  NOT NULL UNIQUE,
-    `designation`  VARCHAR(10) NULL,
-    `standard`     TEXT        NULL,
-    `unit`         INT         NULL
+    `product_no`   BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `product_code` VARCHAR(6)         NOT NULL UNIQUE,
+    `designation`  VARCHAR(45)        NULL,
+    `standard`     TEXT               NULL,
+    `unit`         INT                NULL
 );
+
+INSERT INTO product (product_code,designation,standard,unit) VALUES ('VV0001','pencil','2.5cm * 10cm',10);
 
 CREATE TABLE `customer`
 (
@@ -35,9 +38,11 @@ CREATE TABLE `customer`
 CREATE TABLE `instruction`
 (
     `instruction_no`   VARCHAR(12) NOT NULL,
-    `instruction_date` DATE        NOT NULL,
+    `instruction_date` DATE        NOT NULL DEFAULT NOW(),
     `customer_no`      BIGINT      NOT NULL,
-    `employee_no`      BIGINT      NOT NULL
+    `employee_no`      BIGINT      NOT NULL,
+    `progress_status`  TINYINT(1)  NOT NULL DEFAULT 0,
+    `expiration_date`  DATE        NOT NULL
 );
 
 CREATE TABLE `product_instruction`
@@ -49,39 +54,59 @@ CREATE TABLE `product_instruction`
 
 CREATE TABLE `delivery`
 (
-    `delivery_no` VARCHAR(12) NOT NULL,
-    `status`      TINYINT(1)  NOT NULL,
-    `date`        DATE        NOT NULL,
-    `employee_no` BIGINT      NOT NULL,
-    `customer_no` BIGINT      NOT NULL
+    `delivery_no`     VARCHAR(12) NOT NULL,
+    `progress_status` TINYINT(1)  NOT NULL DEFAULT 0,
+    `date`            DATE        NOT NULL DEFAULT NOW(),
+    `employee_no`     BIGINT      NOT NULL,
+    `customer_no`     BIGINT      NOT NULL
 );
 
 CREATE TABLE `delivery_instruction`
 (
     `delivery_no`    VARCHAR(12) NOT NULL,
     `instruction_no` VARCHAR(12) NOT NULL,
-    `amount`         INT         NULL
+    `amount`         INT         NOT NULL
 );
 
-CREATE TABLE `log`
+CREATE TABLE `employee_log`
 (
     `log_no`      BIGINT      NOT NULL,
-    `type`        TINYINT(1)  NOT NULL,
-    `log_info`    TEXT        NOT NULL,
     `ip_address`  VARCHAR(20) NOT NULL,
-    `date`        DATETIME    NOT NULL,
-    `employee_no` BIGINT      NOT NULL
+    `date`        DATETIME    NOT NULL DEFAULT NOW(),
+    `modifier_no` BIGINT      NOT NULL,
+    `target_no`   BIGINT      NOT NULL,
+    `tpoe`        TINYINT(1)  NOT NULL
 );
 
-ALTER TABLE `employee`
-    ADD CONSTRAINT `PK_EMPLOYEE` PRIMARY KEY (
-                                              `employee_no`
-        );
+CREATE TABLE `product_log`
+(
+    `log_no`      BIGINT      NOT NULL,
+    `ip_address`  VARCHAR(20) NOT NULL,
+    `date`        DATETIME    NOT NULL DEFAULT NOW(),
+    `modifier_no` BIGINT      NOT NULL,
+    `product_no`  BIGINT      NOT NULL,
+    `type`        TINYINT(1)  NOT NULL
+);
 
-ALTER TABLE `product`
-    ADD CONSTRAINT `PK_PRODUCT` PRIMARY KEY (
-                                             `product_no`
-        );
+CREATE TABLE `instruction_log`
+(
+    `log_no`         BIGINT      NOT NULL,
+    `ip_address`     VARCHAR(20) NOT NULL,
+    `date`           DATETIME    NOT NULL DEFAULT NOW(),
+    `modifier_no`    BIGINT      NOT NULL,
+    `instruction_no` VARCHAR(12) NOT NULL,
+    `type`           TINYINT(1)  NOT NULL
+);
+
+CREATE TABLE `delivery_log`
+(
+    `log_no`      BIGINT      NOT NULL,
+    `ip_address`  VARCHAR(20) NOT NULL,
+    `date`        DATETIME    NOT NULL DEFAULT NOW(),
+    `modifier_no` BIGINT      NOT NULL,
+    `delivery_no` VARCHAR(12) NOT NULL,
+    `type`        TINYINT(1)  NOT NULL
+);
 
 ALTER TABLE `customer`
     ADD CONSTRAINT `PK_CUSTOMER` PRIMARY KEY (
@@ -110,26 +135,25 @@ ALTER TABLE `delivery_instruction`
                                                           `instruction_no`
         );
 
-ALTER TABLE `log`
-    ADD CONSTRAINT `PK_LOG` PRIMARY KEY (
-                                         `log_no`
+ALTER TABLE `employee_log`
+    ADD CONSTRAINT `PK_EMPLOYEE_LOG` PRIMARY KEY (
+                                                  `log_no`
         );
 
-# ALTER TABLE `instruction`
-#     ADD CONSTRAINT `FK_customer_TO_instruction_1` FOREIGN KEY (
-#                                                                `customer_no`
-#         )
-#         REFERENCES `customer` (
-#                                `customer_no`
-#             );
-#
-# ALTER TABLE `instruction`
-#     ADD CONSTRAINT `FK_employee_TO_instruction_1` FOREIGN KEY (
-#                                                                `employee_no`
-#         )
-#         REFERENCES `employee` (
-#                                `employee_no`
-#             );
+ALTER TABLE `product_log`
+    ADD CONSTRAINT `PK_PRODUCT_LOG` PRIMARY KEY (
+                                                 `log_no`
+        );
+
+ALTER TABLE `instruction_log`
+    ADD CONSTRAINT `PK_INSTRUCTION_LOG` PRIMARY KEY (
+                                                     `log_no`
+        );
+
+ALTER TABLE `delivery_log`
+    ADD CONSTRAINT `PK_DELIVERY_LOG` PRIMARY KEY (
+                                                  `log_no`
+        );
 #
 # ALTER TABLE `product_instruction`
 #     ADD CONSTRAINT `FK_product_TO_product_instruction_1` FOREIGN KEY (
@@ -145,22 +169,6 @@ ALTER TABLE `log`
 #         )
 #         REFERENCES `instruction` (
 #                                   `instruction_no`
-#             );
-#
-# ALTER TABLE `delivery`
-#     ADD CONSTRAINT `FK_employee_TO_delivery_1` FOREIGN KEY (
-#                                                             `employee_no`
-#         )
-#         REFERENCES `employee` (
-#                                `employee_no`
-#             );
-#
-# ALTER TABLE `delivery`
-#     ADD CONSTRAINT `FK_customer_TO_delivery_1` FOREIGN KEY (
-#                                                             `customer_no`
-#         )
-#         REFERENCES `customer` (
-#                                `customer_no`
 #             );
 #
 # ALTER TABLE `delivery_instruction`
@@ -179,10 +187,3 @@ ALTER TABLE `log`
 #                                   `instruction_no`
 #             );
 #
-# ALTER TABLE `log`
-#     ADD CONSTRAINT `FK_employee_TO_log_1` FOREIGN KEY (
-#                                                        `employee_no`
-#         )
-#         REFERENCES `employee` (
-#                                `employee_no`
-#             );
