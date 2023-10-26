7c19c228281ca0b52004bcab1002d0e3380e2ca2
sangwoong <timeattacks2@naver.com>
2023-10-26 11:46:29 +0900

    REFACTOR : CustomUserDetailsService 코드 수정

--- blooming/src/main/java/com/douzon/blooming/auth/service/CustomUserDetailsService.java
+++ blooming/src/main/java/com/douzon/blooming/auth/service/CustomUserDetailsService.java
@@ -1,13 +1,10 @@
 package com.douzon.blooming.auth.service;
 
+import com.douzon.blooming.auth.EmployeeDetails;
 import com.douzon.blooming.auth.dto.response.EmployeeDto;
 import com.douzon.blooming.auth.exception.NotFoundEmployeeAuthException;
-import com.douzon.blooming.auth.EmployeeDetails;
 import com.douzon.blooming.auth.repo.EmployeeAuthRepository;
-import java.util.Collections;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -32,12 +29,5 @@ public class CustomUserDetailsService implements UserDetailsService {
         employeeDto.getId(),
         employeeDto.getPassword(),
         employeeDto.getRole());
-  }
-
-  private UserDetails createUserDetails(EmployeeDto employeeDto) {
-    return new User(
-        employeeDto.getId(),employeeDto.getPassword(),
-        Collections.singleton(new SimpleGrantedAuthority(employeeDto.getRole().name()))
-    );
   }
 }
