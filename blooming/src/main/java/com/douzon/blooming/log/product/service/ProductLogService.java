57acaec3a5b8051f0046c18e5fbea0197328f969
sangwoong <timeattacks2@naver.com>
2023-10-26 11:47:42 +0900

    FEAT : Log 관련 클래스 추가 및 기능 수정

---
+++ blooming/src/main/java/com/douzon/blooming/log/product/service/ProductLogService.java
@@ -0,0 +1,8 @@
+package com.douzon.blooming.log.product.service;
+
+import com.douzon.blooming.log.product.dto.ProductLogDto;
+
+public interface ProductLogService {
+
+  void addProductLog(ProductLogDto productLogDto);
+}
