21b97c0425b70a240958fd25c5c0b44ef2bdd367
sangwoong <timeattacks2@naver.com>
2023-11-03 09:08:18 +0900

    FEAT : Employee Image 기능 추가

--- blooming/src/main/java/com/douzon/blooming/employee/controller/EmployeeController.java
+++ blooming/src/main/java/com/douzon/blooming/employee/controller/EmployeeController.java
@@ -6,16 +6,24 @@ import com.douzon.blooming.employee.dto.request.LoginEmployeeDto;
 import com.douzon.blooming.employee.dto.request.UpdateEmployeeDto;
 import com.douzon.blooming.employee.dto.response.ResponseEmployeeDto;
 import com.douzon.blooming.employee.dto.response.ResponseListEmployeeDto;
+import com.douzon.blooming.employee.service.EmployeeImageService;
 import com.douzon.blooming.employee.service.EmployeeService;
+import java.net.MalformedURLException;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,9 +31,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
   private final EmployeeService employeeService;
+  private final EmployeeImageService employeeImageService;
 
   @PostMapping("/login")
-  public ResponseEntity<TokenDto> login(@RequestBody LoginEmployeeDto loginEmployeeDto) {
+  public ResponseEntity<TokenDto> login(@RequestBody @Valid LoginEmployeeDto loginEmployeeDto) {
     return ResponseEntity.ok(employeeService.login(loginEmployeeDto));
   }
 
@@ -36,14 +45,39 @@ public class EmployeeController {
 
   @GetMapping("/list")
   public ResponseEntity<ResponseListEmployeeDto> findEmployeeList(
-      @RequestBody EmployeeSearchDto searchDto) {
+      @ModelAttribute EmployeeSearchDto searchDto) {
     return ResponseEntity.ok(employeeService.getEmployeeList(searchDto));
   }
 
   @PutMapping("/{employeeNo}")
   public ResponseEntity<Void> updateEmployee(@PathVariable Long employeeNo,
-      @RequestBody UpdateEmployeeDto updateEmployeeDto) {
+      @RequestBody @Valid UpdateEmployeeDto updateEmployeeDto) {
     employeeService.updateEmployee(updateEmployeeDto, employeeNo);
+    return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping("/{employeeNo}/image")
+  public UrlResource getImage(@PathVariable Long employeeNo) throws MalformedURLException {
+    return employeeImageService.getEmployeeImage(employeeNo);
+  }
+
+  @PostMapping(value = "/{employeeNo}/image", consumes = "multipart/form-data")
+  public ResponseEntity<Void> addImage(@PathVariable Long employeeNo,
+      @RequestParam MultipartFile file) {
+    employeeImageService.addEmployeeImage(file, employeeNo);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PutMapping(value = "/{employeeNo}/image", consumes = "multipart/form-data")
+  public ResponseEntity<Void> modifyImage(@PathVariable Long employeeNo,
+      @RequestParam MultipartFile file) {
+    employeeImageService.updateEmployeeImage(file, employeeNo);
+    return ResponseEntity.noContent().build();
+  }
+
+  @DeleteMapping("/{employeeNo}/image")
+  public ResponseEntity<Void> deleteImage(@PathVariable Long employeeNo) {
+    employeeImageService.deleteEmployeeImage(employeeNo);
     return ResponseEntity.noContent().build();
   }
 }
---
+++ blooming/src/main/java/com/douzon/blooming/employee/service/MemoryEmployeeImageService.java
@@ -0,0 +1,75 @@
+package com.douzon.blooming.employee.service;
+
+import com.douzon.blooming.employee.exception.ImageUploadException;
+import com.douzon.blooming.employee.repo.EmployeeRepository;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class MemoryEmployeeImageService implements
+    EmployeeImageService {
+
+  private static final String ABSOLUTE_PATH = new File("").getAbsolutePath();
+  private static final String PATH = "image";
+  private static final String SEPARATOR = "/";
+
+  private final EmployeeRepository employeeRepository;
+
+  @Override
+  public void addEmployeeImage(MultipartFile multipartFile, Long employeeNo) {
+    String imageUrl = uploadImageInMemory(multipartFile);
+    employeeRepository.updateEmployeeImage(imageUrl, employeeNo);
+  }
+
+  @Override
+  public void updateEmployeeImage(MultipartFile multipartFile, Long employeeNo) {
+    addEmployeeImage(multipartFile, employeeNo);
+  }
+
+  @Override
+  public UrlResource getEmployeeImage(Long employeeNo) throws MalformedURLException {
+    String imageUrl = employeeRepository.findEmployeeImageByEmployeeNo(employeeNo);
+    return new UrlResource(
+        "file :" + ABSOLUTE_PATH + File.separator + PATH + File.separator + imageUrl);
+  }
+
+  @Override
+  public void deleteEmployeeImage(Long employeeNo) {
+    employeeRepository.updateEmployeeImage(null, employeeNo);
+  }
+
+  private String uploadImageInMemory(MultipartFile multipartFile) {
+    if (multipartFile == null || multipartFile.isEmpty()
+        || multipartFile.getContentType() == null) {
+      throw new ImageUploadException("Invalid image file");
+    }
+
+    String contentType = multipartFile.getContentType();
+    String extension;
+
+    if ("image/jpeg".equals(contentType)) {
+      extension = ".jpg";
+    } else if ("image/png".equals(contentType)) {
+      extension = ".png";
+    } else {
+      throw new ImageUploadException("Unsupported image format");
+    }
+
+    UUID uuid = UUID.randomUUID();
+    String fileName = uuid + extension;
+    try {
+      File file = new File(ABSOLUTE_PATH + SEPARATOR + PATH, fileName);
+      multipartFile.transferTo(file);
+      return fileName;
+    } catch (IOException e) {
+      throw new ImageUploadException("Failed to upload image");
+    }
+  }
+}
