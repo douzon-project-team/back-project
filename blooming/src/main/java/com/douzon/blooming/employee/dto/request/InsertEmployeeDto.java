eacf2e8e91a90c321a9e83219ad7acc1248040f2
sangwoong <timeattacks2@naver.com>
2023-11-03 09:05:55 +0900

    FEAT : Log 파일 수정

--- blooming/src/main/java/com/douzon/blooming/employee/dto/request/InsertEmployeeDto.java
+++ blooming/src/main/java/com/douzon/blooming/employee/dto/request/InsertEmployeeDto.java
@@ -21,16 +21,19 @@ public class InsertEmployeeDto {
   private Long employeeNo;
 
   @Pattern(regexp = "^[a-zA-Z0-9]{4,}$")
-  @NotBlank(message = "아이디는 필수 값입니다.")
+  @NotBlank
   private String id;
 
-  @NotBlank(message = "비밀번호는 필수 값입니다.")
+  @NotBlank
   private String password;
-  @NotBlank(message = "이름은 필수 값입니다.")
+  @NotBlank
   private String name;
+
   private EmployeeRole role;
+
   @Pattern(regexp = "^\\d{11}$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
   private String tel;
+
   @Email(message = "이메일 형식이 아닙니다.")
   private String email;
 
