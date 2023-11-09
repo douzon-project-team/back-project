3b15f650ab9f7d8dcf722fc3f34ca1b229e21e3f
sangwoong <timeattacks2@naver.com>
2023-11-09 17:13:07 +0900

    FEAT : sql 문 수정

--- blooming/src/main/resources/_sql.sql
+++ blooming/src/main/resources/_sql.sql
@@ -3,7 +3,6 @@ DROP DATABASE IF EXISTS project;
 CREATE DATABASE IF NOT EXISTS project;
 
 USE project;
-drop table employee;
 CREATE TABLE `employee`
 (
     `employee_no`   BIGINT      NOT NULL PRIMARY KEY,
@@ -16,37 +15,11 @@ CREATE TABLE `employee`
     `email`         VARCHAR(30) NULL
 );
 
-INSERT INTO `employee` (employee_no, id, password, name, img, role, tel, email)
-VALUES (200001, 'admin', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200002, 'admin1', 'admin1', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200003, 'admin2', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200004, 'admin3', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200005, 'admin4', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200006, 'admin5', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200007, 'admin6', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200008, 'admin7', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200009, 'admin8', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200010, 'admin9', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200011, 'admin10', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
-        '01011112222', 'admin@admin.com'),
-       (200012, 'member', 'member', 'member', '4287e7f7-17a8-4710-b32f-d21255ea53e2.png', 1,
-        '01022223333', 'member@member.com');
-
 CREATE TABLE `product`
 (
     `product_no`    BIGINT             PRIMARY KEY AUTO_INCREMENT,
     `product_code`  VARCHAR(6)         NOT NULL UNIQUE,
-    `product_name`  VARCHAR(10)        NOT NULL,
+    `product_name`  VARCHAR(45)        NOT NULL,
     `standard`      TEXT               NOT NULL,
     `unit`          INT                NOT NULL
 );
@@ -73,9 +46,6 @@ CREATE TABLE `customer`
     `customer_tel`     VARCHAR(13) NOT NULL
 );
 
-INSERT INTO customer VALUES (1, 'C0001', 'samsung', '010-1234-1234'),
-                            (2, 'C0002', 'Lg', '010-3214-3214'),
-                            (3, 'C0003', 'Douzone', '010-4444-4444');
 
 CREATE TABLE `instruction`
 (
@@ -87,9 +57,6 @@ CREATE TABLE `instruction`
     `expiration_date`  DATE        NOT NULL
 );
 
-INSERT INTO instruction(instruction_date, customer_no, employee_no, expiration_date) VALUES ('2023-10-22', 3, 200003, '2023-11-21'),
-                                                                                            ('2023-10-25', 2, 200005, '2023-11-25');
-
 CREATE TABLE `product_instruction`
 (
     `product_no`     BIGINT      NOT NULL,
@@ -98,10 +65,7 @@ CREATE TABLE `product_instruction`
     `remain_amount`  INT         NOT NULL
 );
 
-INSERT INTO product_instruction VALUES (1, 'WO2311000001', 15, 5),
-                                       (2, 'WO2311000001', 20, 5),
-                                       (1, 'WO2311000002', 15, 10),
-                                       (1, 'WO2311000002', 30, 20);
+
 
 CREATE TABLE `delivery`
 (
@@ -111,7 +75,6 @@ CREATE TABLE `delivery`
     `employee_no`     BIGINT      NOT NULL
 );
 
-INSERT INTO delivery(delivery_date, employee_no) VALUES ('2023-11-29', 200004);
 
 CREATE TABLE `delivery_instruction`
 (
@@ -120,11 +83,6 @@ CREATE TABLE `delivery_instruction`
     `product_no`     BIGINT      NOT NULL,
     `amount`         INT         NOT NULL
 );
-
-INSERT INTO delivery_instruction VALUES ('MW2311000001', 'WO2311000001', 1, 10),
-                                        ('MW2311000001', 'WO2311000001', 2, 15),
-                                        ('MW2311000001', 'WO2311000002', 1, 5),
-                                        ('MW2311000001', 'WO2311000002', 2, 10);
 
 CREATE TABLE `employee_log`
 (
@@ -237,35 +195,6 @@ END;
 //
 DELIMITER ;
 
-# Delivery 등록시 instruction의 잔량, 진행 상태 변화 Trigger
-drop trigger insert_remain_amount;
-DELIMITER //
-CREATE TRIGGER insert_remain_amount
-    AFTER INSERT ON delivery_instruction
-    FOR EACH ROW
-BEGIN
-    DECLARE new_remain_amount INT;
-    SET new_remain_amount = (SELECT remain_amount - NEW.amount FROM product_instruction
-                             WHERE instruction_no = NEW.instruction_no AND product_no = NEW.product_no);
-
-    UPDATE product_instruction
-    SET remain_amount = new_remain_amount
-    WHERE instruction_no = NEW.instruction_no AND product_no = NEW.product_no;
-
-    UPDATE instruction
-    SET progress_status = 1
-    WHERE instruction_no = NEW.instruction_no;
-
-    IF (SELECT COUNT(*) FROM product_instruction
-        WHERE instruction_no = NEW.instruction_no
-        AND remain_amount > 0) = 0 THEN
-        UPDATE instruction
-        SET progress_status = 2
-        WHERE instruction_no = NEW.instruction_no;
-    end if;
-END;
-//
-DELIMITER ;
 
 # Delivery 수정시 inistruction의 잔량, 진행 상태 변화 Trigger
 # remain_amount = remain_amount + Old.amount - new.amount
@@ -290,7 +219,6 @@ END;
 DELIMITER ;
 
 # Delivery 삭제시 instruction의 잔량, 진행 상태 변화 Trigger
-drop trigger delete_restore_remain_amount;
 DELIMITER //
 CREATE TRIGGER delete_restore_remain_amount
     AFTER DELETE ON delivery_instruction
@@ -313,4 +241,79 @@ BEGIN
         WHERE instruction_no = OLD.instruction_no;
     END IF;
 END;
+DELIMITER ;
+INSERT INTO `employee` (employee_no, id, password, name, img, role, tel, email)
+VALUES (200001, 'admin', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200002, 'admin1', 'admin1', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200003, 'admin2', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200004, 'admin3', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200005, 'admin4', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200006, 'admin5', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200007, 'admin6', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200008, 'admin7', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200009, 'admin8', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200010, 'admin9', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200011, 'admin10', 'admin', 'admin', '4bce5389-8191-46f8-ad91-a5ee197ee837.png', 0,
+        '01011112222', 'admin@admin.com'),
+       (200012, 'member', 'member', 'member', '4287e7f7-17a8-4710-b32f-d21255ea53e2.png', 1,
+        '01022223333', 'member@member.com');
+
+
+INSERT INTO customer VALUES (1, 'C0001', 'samsung', '010-1234-1234'),
+                            (2, 'C0002', 'Lg', '010-3214-3214'),
+                            (3, 'C0003', 'Douzone', '010-4444-4444');
+
+
+INSERT INTO instruction(instruction_date, customer_no, employee_no, expiration_date) VALUES ('2023-10-22', 3, 200003, '2023-11-21'),
+                                                                                            ('2023-10-25', 2, 200005, '2023-11-25');
+
+INSERT INTO delivery(delivery_date, employee_no) VALUES ('2023-11-29', 200004);
+
+INSERT INTO product_instruction VALUES (1, 'WO2311000001', 15, 5),
+                                       (2, 'WO2311000001', 20, 5),
+                                       (1, 'WO2311000002', 15, 10),
+                                       (1, 'WO2311000002', 30, 20);
+
+INSERT INTO delivery_instruction VALUES ('MW2311000001', 'WO2311000001', 1, 10),
+                                        ('MW2311000001', 'WO2311000001', 2, 15),
+                                        ('MW2311000001', 'WO2311000002', 1, 5),
+                                        ('MW2311000001', 'WO2311000002', 2, 10);
+
+# Delivery 등록시 instruction의 잔량, 진행 상태 변화 Trigger
+DELIMITER //
+CREATE TRIGGER insert_remain_amount
+    AFTER INSERT ON delivery_instruction
+    FOR EACH ROW
+BEGIN
+    DECLARE new_remain_amount INT;
+    SET new_remain_amount = (SELECT remain_amount - NEW.amount FROM product_instruction
+                             WHERE instruction_no = NEW.instruction_no AND product_no = NEW.product_no);
+
+    UPDATE product_instruction
+    SET remain_amount = new_remain_amount
+    WHERE instruction_no = NEW.instruction_no AND product_no = NEW.product_no;
+
+    UPDATE instruction
+    SET progress_status = 1
+    WHERE instruction_no = NEW.instruction_no;
+
+    IF (SELECT COUNT(*) FROM product_instruction
+        WHERE instruction_no = NEW.instruction_no
+          AND remain_amount > 0) = 0 THEN
+        UPDATE instruction
+        SET progress_status = 2
+        WHERE instruction_no = NEW.instruction_no;
+    end if;
+END;
+//
 DELIMITER ;
