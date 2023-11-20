1402f45058de47f7505b751b004a9921ba2663c8
geun-7 <worms502@gmail.com>
2023-11-20 14:15:45 +0900

    FEAT: 중복 체크로직을 위한 Dto 추가

---
+++ blooming/src/main/java/com/douzon/blooming/customer/dto/request/DuplicateRequestDto.java
@@ -0,0 +1,12 @@
+package com.douzon.blooming.customer.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DuplicateRequestDto {
+    private String customerCode;
+}
---
+++ blooming/src/main/java/com/douzon/blooming/customer/dto/response/ResponseDuplicateResultDto.java
@@ -0,0 +1,10 @@
+package com.douzon.blooming.customer.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDuplicateResultDto {
+    private boolean duplicateResult;
+}
