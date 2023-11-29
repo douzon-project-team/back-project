9b7519479184ff5c3a79bc42dc83a3bce13c2386
geun-7 <worms502@gmail.com>
2023-11-29 13:42:31 +0900

    FEAT: 품목코드 중복체크 Dto 추가

---
+++ blooming/src/main/java/com/douzon/blooming/product/dto/request/ProductCodeCheckDto.java
@@ -0,0 +1,10 @@
+package com.douzon.blooming.product.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ProductCodeCheckDto {
+    private String productCode;
+}
