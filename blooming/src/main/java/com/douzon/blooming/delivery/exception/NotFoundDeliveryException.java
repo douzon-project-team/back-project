45adc85341dcdceffeda5287fc7f3695c9af2e79
geun-7 <worms502@gmail.com>
2023-12-11 21:15:05 +0900

    REFACTOR: 지시, 출고 Exception 수정으로 인한 삭제

--- blooming/src/main/java/com/douzon/blooming/delivery/exception/NotFoundDeliveryException.java
+++
@@ -1,7 +0,0 @@
-package com.douzon.blooming.delivery.exception;
-
-public class NotFoundDeliveryException extends RuntimeException{
-    public NotFoundDeliveryException() {
-        super("Delivery not found");
-    }
-}
--- blooming/src/main/java/com/douzon/blooming/instruction/exception/NotFoundInstructionException.java
+++
@@ -1,7 +0,0 @@
-package com.douzon.blooming.instruction.exception;
-
-public class NotFoundInstructionException extends RuntimeException {
-    public NotFoundInstructionException() {
-        super("Instruction not found");
-    }
-}
