75a91bdd17e59e72f10282d3ec457e8f95d708f6
sangwoong <timeattacks2@naver.com>
2023-12-14 14:02:28 +0900

    FEAT : 사용하지 않는 예외 삭제

--- blooming/src/main/java/com/douzon/blooming/delivery/exception/NotFoundDeliveryException.java
+++
@@ -1,7 +0,0 @@
-package com.douzon.blooming.delivery.exception;
-
-public class NotFoundDeliveryException extends RuntimeException{
-    public NotFoundDeliveryException() {
-        super("해당 출고를 찾을수 없습니다.");
-    }
-}
--- blooming/src/main/java/com/douzon/blooming/instruction/exception/NotFoundInstructionException.java
+++
@@ -1,7 +0,0 @@
-package com.douzon.blooming.instruction.exception;
-
-public class NotFoundInstructionException extends RuntimeException {
-    public NotFoundInstructionException() {
-        super("해당 지시를 찾을 수 없습니다.");
-    }
-}
