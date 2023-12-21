d433ec084ea63166bb94f02f6e5f4746ac4d646f
sangwoong <timeattacks2@naver.com>
2023-12-21 11:28:35 +0900

    FEAT : 유호성 체크 추가

--- blooming/src/main/java/com/douzon/blooming/instruction/dto/request/UpdateInstructionDto.java
+++ blooming/src/main/java/com/douzon/blooming/instruction/dto/request/UpdateInstructionDto.java
@@ -1,6 +1,8 @@
 package com.douzon.blooming.instruction.dto.request;
 
 import java.time.LocalDate;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,7 +15,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 @AllArgsConstructor
 public class UpdateInstructionDto {
   @Setter
+  @Max(12)
   private String instructionNo;
+  @NotBlank(message = "사원 번호는 필수입니다.")
   private Long customerNo;
   @DateTimeFormat(pattern = "yyyy-MM-dd")
   private LocalDate instructionDate;
--- blooming/src/main/java/com/douzon/blooming/product/dto/request/InsertProductDto.java
+++ blooming/src/main/java/com/douzon/blooming/product/dto/request/InsertProductDto.java
@@ -1,5 +1,6 @@
 package com.douzon.blooming.product.dto.request;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -17,14 +18,18 @@ public class InsertProductDto {
   @Pattern(regexp = "^[a-zA-Z]{2}\\d{4}$")
   private String productCode;
   @NotBlank(message = "명칭은 필수 값입니다.")
+  @Max(45)
   private String productName;
   @NotBlank(message = "규격은 필수 값입니다.")
   private String standard;
   @NotNull(message = "개수는 필수 값입니다.")
+  @Max(Integer.MAX_VALUE)
   private int unit;
   @NotNull(message = "무게는 필수 값입니다.")
+  @Max(Integer.MAX_VALUE)
   private int weight;
   @NotNull(message = "가격은 필수 값입니다.")
+  @Max(Integer.MAX_VALUE)
   private int price;
 
 }
